e2bcc19af6cde31683a3c4fea62a6e324450027d
LuisSic <lantoni24@hotmail.com>
2020-08-13 01:57:45 -0600

    Updates commonlib

--- commonlib/src/index.ts
+++ commonlib/src/index.ts
@@ -9,3 +9,11 @@ export * from './errors/request-validation-error';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+
+//Re-export stuff from events
+export * from './events/Types/order-status';
+export * from './events/base-publisher';
+export * from './events/payment-created-event';
+export * from './events/product-created-event';
+export * from './events/product-updated-event';
+export * from './events/subjects';
